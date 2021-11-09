5f741b5861579b385da8c50449713ad1fa211e7b
smulCode <67417862+smulCode@users.noreply.github.com>
2021-11-09 14:27:17 +0100

    V1. Update

---
+++ js/main.js
@@ -0,0 +1,50 @@
+'use strict';
+
+//Hamburger-menu-nav
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+//navbar scroll events
+
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".main");
+
+
+const sectionOneOptions = {
+  rootMargin: "-500px 0px 0px 0px",
+};
+
+const sectionOneObservers = new IntersectionObserver(function (
+  entries,
+  sectionOneObservers
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObservers.observe(sectionOne);
+
+
